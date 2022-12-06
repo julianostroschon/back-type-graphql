2ea9cf491e432a24481689253663a420b934c771
Juliano Stroschon <julianostroschon@gmail.com>
2022-12-06 14:04:04 -0300

    ✨ docker-compose configurado e funcionando

--- src/infra/database/database-config.ts
+++ src/infra/database/database-config.ts
@@ -5,11 +5,11 @@ const getConfig = (configName: string, fallback: string | undefined = undefined)
 const DATABASE = Object.freeze({
   prefix: getConfig('DB_PREFIX', ''),
   ssl: getConfig('DB_SSL'),
-  port: getConfig('DB_PORT', '5432'),
+  port: getConfig('DB_PORT', '4432'),
   host: getConfig('DB_HOST', '127.0.0.1'),
   user: getConfig('DB_USER', 'root'),
   password: getConfig('DB_PASSWORD', 'root'),
-  name: getConfig('DB_NAME', 'local')
+  name: getConfig('DB_NAME', 'posrgres')
 })
 
 export { DATABASE }
