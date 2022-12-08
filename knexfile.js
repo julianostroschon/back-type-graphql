aae434edb7312209c03383d6aba058041e9c573b
Andre Luis <andre.1357.luis@gmail.com>
2022-12-08 11:23:07 -0300

    🔧 add knexfile

---
+++ knexfile.js
@@ -0,0 +1,33 @@
+require('ts-node').register();
+
+const path = require('node:path');
+const { getKnexConfig } = require('./src/infra/database/config');
+
+/**
+ *
+ * @param {string} dest - destination path
+ * @returns
+ */
+function absolute(dest) {
+  return path.join(__dirname, dest);
+}
+
+module.exports = () => {
+  const dbname = process.env.DB_NAME ?? 'postgres';
+
+  const extra = {
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: absolute('./src/database/knex/migrations'),
+      tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: absolute('./src/database/knex/seeds')
+    }
+  };
+
+  return getKnexConfig(dbname, extra);
+};
