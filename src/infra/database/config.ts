ad27ffe29646cd5cef1e9fe35c99ab75e5081bd8
Andre Luis <andre.1357.luis@gmail.com>
2022-12-08 11:24:23 -0300

    ✨ arquivo de configuração para o knex

---
+++ src/infra/database/config.ts
@@ -0,0 +1,18 @@
+import { Knex } from 'knex';
+import { defaultsDeep } from 'lodash';
+
+const config = Object.freeze({
+  client: 'pg',
+  connection: {
+    host: process.env.DB_HOST ?? 'localhost',
+    port: process.env.DB_PORT ?? '4431',
+    user: process.env.DB_USER ?? 'root',
+    password: process.env.DB_PASSWORD ?? 'root',
+    database: process.env.DB_NAME ?? 'postgres'
+  },
+  pool: { min: 0, max: 5, idleTimeoutMillis: 60000 }
+});
+
+export function getKnexConfig(database: string, extra = {}): Knex.Config {
+  return defaultsDeep({ connection: { database } }, extra, config) as Knex.Config;
+}
