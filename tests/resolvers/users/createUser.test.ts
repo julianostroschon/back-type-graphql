423f232dc0b8d396b92a2a8f24ca50cc96f203db
Juliano Stroschon <julianostroschon@gmail.com>
2022-12-11 10:10:04 -0300

    ✅ add testes de getUser e createUSer

---
+++ tests/resolvers/users/createUser.test.ts
@@ -0,0 +1,34 @@
+import { buildDbMock } from '../../../src/support/database';
+import { UserResolver } from '../../../src/server/resolvers/user';
+
+describe('Resolver getUser', () => {
+  test('Deve retornar usuário com o id 3', () => {
+    const { createUser } = new UserResolver();
+    const user = { id: '2', name: 'Juliano', email: 'arrobinha' };
+    const { database, queries } = buildDbMock(
+      Promise.resolve([{ id: '1', name: 'u Homem Macaco', email: 'arrobinha@dosguri' }])
+    );
+
+    const ctx = { database, user };
+    const [query] = queries;
+
+    const args = { name: 'u Homem Macaco', email: 'arrobinha@dosguri' };
+
+    expect(createUser({}, args, ctx)).resolves.toEqual({
+      id: '1',
+      name: 'u Homem Macaco',
+      email: 'arrobinha@dosguri'
+    });
+
+    expect(database).toHaveBeenCalledTimes(1);
+    expect(query.insert).toHaveBeenCalledTimes(1);
+    expect(query.where).not.toHaveBeenCalled();
+    expect(query.first).not.toHaveBeenCalled();
+
+    expect(database).toHaveBeenCalledWith('users');
+    expect(query.insert).toHaveBeenCalledWith(
+      { email: 'arrobinha@dosguri', name: 'u Homem Macaco' },
+      ['*']
+    );
+  });
+});
---
+++ tests/resolvers/users/getUser.test.ts
@@ -0,0 +1,31 @@
+import { buildDbMock } from '../../../src/support/database';
+import { UserResolver } from '../../../src/server/resolvers/user';
+
+describe('Resolver getUser', () => {
+  test('Deve retornar usuário com o id 3 `pong`', () => {
+    const { getUser } = new UserResolver();
+    const user = { id: '2', name: 'Juliano', email: 'arrobinha' };
+    const { database, queries } = buildDbMock(
+      Promise.resolve({ id: '3', name: 'Jesualdo', email: 'arrobinha@dosguri' })
+    );
+
+    const ctx = { database, user };
+    const [query] = queries;
+
+    const id = '3';
+
+    expect(getUser({}, id, ctx)).resolves.toEqual({
+      id: '3',
+      name: 'Jesualdo',
+      email: 'arrobinha@dosguri'
+    });
+
+    expect(database).toHaveBeenCalledTimes(1);
+    expect(query.where).toHaveBeenCalledTimes(1);
+    expect(query.first).toHaveBeenCalledTimes(1);
+
+    expect(database).toHaveBeenCalledWith('users');
+    expect(query.where).toHaveBeenCalledWith({ id: '3' });
+    expect(query.first).toHaveBeenCalledWith();
+  });
+});
