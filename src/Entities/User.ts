882761f8d8aab83477201af7ca51e07b5c4906bc
Juliano Stroschon <julianostroschon@gmail.com>
2022-12-12 09:31:19 -0300

    ♻️ refator meio que geral

--- src/Entities/User.ts
+++ src/Entities/User.ts
@@ -3,14 +3,14 @@ import { Field, ObjectType } from 'type-graphql'
 @ObjectType()
 export class User {
   @Field()
-  id!: string
+  id: string
 
   @Field()
-  name!: string
+  name: string
 
   @Field({ nullable: false })
-  email!: string
+  email: string
 
   @Field({ nullable: false })
-  password?: string
+  password: string
 }
