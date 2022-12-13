28887b71851851dd5bd0eb68bfcbd555daac9384
Andre Luis <andre.1357.luis@gmail.com>
2022-12-13 10:35:08 -0300

    🦺 tipagem para buildResolvers

--- src/server/buildResolvers.ts
+++ src/server/buildResolvers.ts
@@ -1,12 +1,13 @@
 import { loadFiles } from '@graphql-tools/load-files';
+import { TypeResolvers } from './types';
 import path from 'path';
 
-export const buildResolvers = async (): Promise<any[]> => {
+export const buildResolvers = async (): Promise<TypeResolvers> => {
   const resolversArrayBundle = await loadFiles(
     path.join(__dirname, './resolvers/**/')
   );
 
   return resolversArrayBundle.map((resolver: object[]) => {
     return Object.values(resolver)[0];
-  });
+  }) as TypeResolvers;
 };
