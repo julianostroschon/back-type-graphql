9721dfa6347992b67fc0f74c7a3f58edcc12ee78
Andre Luis <andre.1357.luis@gmail.com>
2022-12-13 10:35:20 -0300

    ♻️ retorno explícito para runServer

--- src/index.ts
+++ src/index.ts
@@ -10,11 +10,13 @@ const PORT = getPort();
  * We create a new ApolloServer instance, passing in the result of calling createConfigServer() as the
  * configuration object
  */
-async function runServer() {
+async function runServer(): Promise<void> {
   const server = new ApolloServer(await createConfigServer());
 
   const { url } = await server.listen(PORT);
+
   console.log(`🚀\nServer ready at ${url}`);
 }
 
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 runServer();
