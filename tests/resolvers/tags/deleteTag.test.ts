8ad0d79f9ddb0eacb04ebbe23b5ebf3b0e4eef25
Juliano Stroschon <julianostroschon@gmail.com>
2022-12-21 15:09:29 -0300

    ✅ att teste deleteTag

--- tests/resolvers/tags/deleteTag.test.ts
+++ tests/resolvers/tags/deleteTag.test.ts
@@ -1,25 +1,24 @@
-import { buildDbMock } from '../../../src/support/database';
-import { TagResolver } from '../../../src/server/resolvers/tag';
+import { buildCtxMock } from '../../stub-context';
+import { TagResolver } from '../../../src/resolvers/tag';
 
-describe('Resolver readUser', () => {
-  test('Deve deletar o usuario', () => {
+describe('DeleteTag', () => {
+  test('Deve deletar a tag', () => {
     const { deleteTag } = new TagResolver();
-    const user = { id: '2', name: 'Juliano', email: 'arrobinha' };
-    const { database, queries } = buildDbMock(
+
+    const { ctx, queries } = buildCtxMock(
       Promise.resolve([{ id: '1', name: 'dev', description: 'typefs' }])
     );
 
-    const ctx = { database, user };
     const [query] = queries;
     const id = '2';
 
     expect(deleteTag({}, id, ctx)).resolves.toEqual(true);
 
-    expect(database).toHaveBeenCalledTimes(1);
+    expect(ctx.database).toHaveBeenCalledTimes(1);
     expect(query.delete).toHaveBeenCalledTimes(1);
     expect(query.where).toHaveBeenCalledTimes(1);
 
-    expect(database).toHaveBeenCalledWith('tags');
+    expect(ctx.database).toHaveBeenCalledWith('tags');
     expect(query.delete).toHaveBeenCalledWith();
     expect(query.where).toHaveBeenCalledWith({ id: '2' });
   });
